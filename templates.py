89331d5538c6836393e3dcc323995d6cebc30dd4
007kakashi <upendrasgosavi@gmail.com>
2023-10-11 19:26:41 +0530

    templates has been created

---
+++ templates.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s')
+
+project_name="Facial_Emotion_Recognition"
+
+list_of_files=[
+
+    '.github/workflow/.gitkeep',
+    f'src/{project_name}/__init__.py',
+    f'src/{project_name}/components/__init__.py',
+    f'src/{project_name}/utils/__init__.py',
+    f'src/{project_name}/pipelines/__init__.py',
+    f'src/{project_name}/config/__init__.py',
+    f'src/{project_name}/config/configration.py',
+    f'src/{project_name}entity/__init__.py',
+    f'src/{project_name}constant/__init__.py',
+    'config/config.yaml',
+    'dvc.yaml',
+    'params.yaml',
+    'requirements.txt',
+    'setup.py',
+    'research/trails.ipynb'
+
+
+]
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+
+    filedir ,filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f'Creating directory {filedir} for file : {filename}')
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,'w') as f:
+            pass
+
+        logging.info(f'Creating empty file: {filepath}')
+
+    else:
+        logging.info(f'{filename} already exist')
