11f402e2b9e338b2cea2c742f5ee0f791cba2cda
007kakashi <upendrasgosavi@gmail.com>
2023-10-12 13:26:45 +0530

    updated

--- templates.py
+++ templates.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s')
 
-project_name="Facial_Emotion_Recognition_End2End"
+project_name="Facial_Emotion_Recognition"
 
 list_of_files=[
 
